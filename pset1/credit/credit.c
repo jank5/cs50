278297af65c18186a4ed44ecfce51d39b346f4a7
jank5 <klevzhic5jank@gmail.com>
2025-08-08 17:17:44 +0300

    update code credit.c

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -2,14 +2,27 @@
 #include <string.h>
 #include "../../lib/cs50.h"
 
+int multiply_and_addition(int length, string number_card);
+void what_is_card(int result, string number_card);
+
 int main(void)
 {
-    int sum, result, sum_check;
-    string x = get_string("Number: ");
-    int length = strlen(x);
+    int result, length;
+    string number_card = get_string("Number: ");
+    length = strlen(number_card);
+    result = multiply_and_addition(length, number_card);
+    what_is_card(result, number_card);
+
+    return 0;
+}
+
+int multiply_and_addition(int length, string number_card)
+{
+    int sum = 0;
+    int result = 0;
     for (int i = 2; i <= length; i+=2)
     {
-        int output = x[length - i] - '0';
+        int output = number_card[length - i] - '0';
         sum = output * 2;
         if (sum >= 10){
             int a,b;
@@ -23,22 +36,27 @@ int main(void)
     }
     for (int j = 1; j <= length; j+=2)
     {
-        int output = x[length - j] - '0';
+        int output = number_card[length - j] - '0';
         sum = output;
         result += sum;
     }
-    int sum_numb;
+    return result;
+}
+
+void what_is_card(int result, string number_card)
+{
+    int length = strlen(number_card);
     if (result % 10 == 0)
     {
-        if (strncmp(x,"4",1) == 0)
+        if (length == 13 || length == 16 && strncmp(number_card,"4",1) == 0)
         {
             printf("VISA\n");
         }
-        else if (strncmp(x,"34",2) == 0 || strncmp(x,"37",2) == 0)
+        else if (length == 15 && strncmp(number_card,"34",2) == 0 || strncmp(number_card,"37",2) == 0)
         {
             printf("AMEX\n");
         }
-        else if (strncmp(x,"51",2) == 0 || strncmp(x,"52",2) == 0 || strncmp(x,"53",2) == 0 || strncmp(x,"54",2) == 0 || strncmp(x,"55",2) == 0)
+        else if (length == 16 && strncmp(number_card,"51",2) == 0 || strncmp(number_card,"52",2) == 0 || strncmp(number_card,"53",2) == 0 || strncmp(number_card,"54",2) == 0 || strncmp(number_card,"55",2) == 0)
         {
             printf("MASTERCARD\n");
         }
@@ -47,5 +65,4 @@ int main(void)
     {
         printf("INVALID\n");
     }
-    return 0;
 }
